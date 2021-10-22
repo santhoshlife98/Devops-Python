52654340b34f0b752184c30ea0e066a243904362
Santhosh <santhoshlife98@gmail.com>
2021-10-23 02:18:14 +0530

    Create vowels_consonants.py

---
+++ vowels_consonants.py
@@ -0,0 +1,20 @@
+''' Author @santhosh.bheeman '''
+
+
+
+def check_vowels_consonants(s):
+    vow = []
+    cons = []
+    for i in s:
+        #print(i)
+        if i in "aeiou":
+            vow.append(i)
+        elif i not in "aeiuo":
+            cons.append(i)
+
+    print("The vowels are:",''.join(vow))
+    print("The consonants are:",''.join(cons))
+
+if __name__ == '__main__':
+    s=input("Enter the string: ")
+    check_vowels_consonants(s)
